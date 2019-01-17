bab82c778a2ca2101039aff78bc9e0fd51f29d34
disburden <disburden.gamil.com>
2019-01-17 15:26:13 +0800

    add cornerRadius

--- lib/CommWidget/FCTextView.dart
+++ lib/CommWidget/FCTextView.dart
@@ -9,46 +9,54 @@ class FCTextView extends StatefulWidget {
 	Color backgroundColor;
 	TextStyle hintStyel;
 	String hintStr;
-	EdgeInsets gap; /// 文字距离上下左右的边距
+	EdgeInsets gap;
+	double cornerRadius;
 
-	FCTextView({@required this.height,this.width,
+	/// 文字距离上下左右的边距
+
+	FCTextView({@required this.height, this.width,
 		TextEditingController controller,
 		this.textChanged,
 		this.textStyle,
 		this.backgroundColor,
 		this.hintStyel,
-		this.hintStr
-	}){
+		this.hintStr,
+		this.gap,
+		this.cornerRadius
+	}) {
 		_contrl == controller ?? TextEditingController();
 	}
 
-  @override
-  _FCTextViewState createState() => _FCTextViewState();
+	@override
+	_FCTextViewState createState() => _FCTextViewState();
 }
 
 class _FCTextViewState extends State<FCTextView> {
 
-  @override
-  Widget build(BuildContext context) {
-    return  Container(
-	    color: widget.backgroundColor,
-	    constraints: BoxConstraints(
-		    maxHeight: widget.height,
-		    maxWidth: widget.width,
-		    minHeight: widget.height,
-		    minWidth: widget.width),
-	    padding: widget.gap,
-	    child: TextField(
-		    maxLines: null,
-		    keyboardType: TextInputType.multiline,
-		    controller: widget._contrl,
-		    style: widget.textStyle,
-		    decoration: InputDecoration.collapsed(
-			    hintText: widget.hintStr,
-			    hintStyle: widget.hintStyel
-		    ),
-		    onChanged: widget.textChanged,
-	    ),
-    );
-  }
+	@override
+	Widget build(BuildContext context) {
+		return ClipRRect(
+			borderRadius: BorderRadius.circular(widget.cornerRadius),
+			child: Container(
+				color: widget.backgroundColor,
+				constraints: BoxConstraints(
+					maxHeight: widget.height,
+					maxWidth: widget.width,
+					minHeight: widget.height,
+					minWidth: widget.width),
+				padding: widget.gap,
+				child: TextField(
+					maxLines: null,
+					keyboardType: TextInputType.multiline,
+					controller: widget._contrl,
+					style: widget.textStyle,
+					decoration: InputDecoration.collapsed(
+						hintText: widget.hintStr,
+						hintStyle: widget.hintStyel
+					),
+					onChanged: widget.textChanged,
+				),
+			),
+		);
+	}
 }
