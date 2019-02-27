3c5bfc2ee181b607201c8aad51c3fdc08bda9b98
disburden <disburden.gamil.com>
2019-02-27 13:52:12 +0800

    增加选择日期的控件

--- lib/CommFunction/FCEncrypt.dart
+++ lib/CommFunction/FCEncrypt.dart
@@ -6,12 +6,13 @@ import 'package:encrypt/encrypt.dart';
 
 class FCEncrypt{
 
+	/// Aes加解密
 	static String encryptUseAes(String originText,String keyStr) {
 		final key = Key.fromUtf8(keyStr);
 		final iv = IV.fromLength(16);
 		final encrypter = Encrypter(AES(key, iv,mode: AESMode.ecb));
 
-		// encrypted是加密后的Encrypted对象,并不是String,需要用base64转成字符串
+		// encrypted是加密后的Encrypted对象,并不是String,需要用base64方法获取字符串
 		final encrypted = encrypter.encrypt(originText);
 		return encrypted.base64;
 	}
@@ -27,6 +28,7 @@ class FCEncrypt{
 		return encrypter.decrypt(encrypted);
 	}
 
+	/// 3des加解密(注意:对中文支持有点问题)
 	static String encryptUse3des(String originText,String keyStr) {
 		var blockCipher = new BlockCipher(new DESEngine(), keyStr);
 		return blockCipher.encodeB64(originText);
@@ -37,12 +39,14 @@ class FCEncrypt{
 		return blockCipher.decodeB64(encryptedStr);
 	}
 
+	/// md5
 	static String encryptUseMd5(String data) {
 		List<int> intList = utf8.encode(data);
 		var result = md5.convert(intList);
 		return result.toString();
 	}
 
+	/// base64加解密
 	static String encodeUseBase64(String data){
 		Uint8List u8Str = Uint8List.fromList(data.codeUnits);
 		String base64Str = base64Encode(u8Str);
--- lib/CommWidget/FCDialog.dart
+++ lib/CommWidget/FCDialog.dart
@@ -210,3 +210,16 @@ inputDialog(BuildContext context, String message,bool isPass, InputDone onEnsure
 		)
 	);
 }
+
+
+ValueChanged<DateTime> selectDate;
+
+Future<void> pickerDate(BuildContext context,ValueChanged<DateTime> selectDate) async {
+	final DateTime picked = await showDatePicker(
+		context: context,
+		initialDate: DateTime.now(),
+		firstDate: DateTime(2015, 8),
+		lastDate: DateTime(2101)
+	);
+	selectDate(picked);
+}
