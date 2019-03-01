2a41992cd1d1dec10d8c449b9bcdcc4edf49857d
disburden <disburden.gamil.com>
2019-03-01 15:53:47 +0800

    .组件化dialog模块

---
+++ lib/CommDialog/CoreDialog.dart
@@ -0,0 +1,5 @@
+/// 一些常用的函数合集
+
+library CoreDialog;
+
+export 'FCPicker.dart';
--- lib/flutter_comm.dart
+++ lib/flutter_comm.dart
@@ -3,4 +3,4 @@ library flutter_comm;
 
 export 'package:flutter_comm/CommFunction/CoreFunction.dart';
 export 'package:flutter_comm/CommWidget/CoreWidget.dart';
-
+export 'package:flutter_comm/CommDialog/CoreDialog.dart';
