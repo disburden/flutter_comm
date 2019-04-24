a27c31e6e67d98809073b0a51f4a6a5f77218385
disburden <disburden.gamil.com>
2019-04-24 14:25:50 +0800

    将dart相关的内容单独出来,创建为新的共享项目

---
+++ lib/CommWidget/FCWidgetUtils.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+class FCWidgetUtils {
+	
+	/// 画一条分割线
+	static Widget splitLine({
+		double height = 8.0,
+		double leftIndent = 0.0,
+		double rightIndent = 0.0,
+		Color lineColor = const Color(0xfff7f7f7)
+	}) {
+		return Padding(
+			padding: EdgeInsets.only(left: leftIndent, right: rightIndent),
+			child: Container(
+				color: lineColor,
+				height: height,
+			),
+		);
+	}
+	
+	/// 生成一个随机颜色
+	static Color sjColor() {
+		math.Random random = math.Random();
+		return Color.fromARGB(255, random.nextInt(255), random.nextInt(255), random.nextInt(255));
+	}
+}
--- lib/flutter_comm.dart
+++ lib/flutter_comm.dart
@@ -1,6 +1,6 @@
 library flutter_comm;
 
 
-export 'package:flutter_comm/CommFunction/CoreFunction.dart';
+//export 'package:flutter_comm/CommFunction/CoreFunction.dart';
 export 'package:flutter_comm/CommWidget/CoreWidget.dart';
 export 'package:flutter_comm/CommDialog/CoreDialog.dart';
