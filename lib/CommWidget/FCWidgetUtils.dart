d9d40e321eec6d8c5acfacbb50c5eb5fd0963310
disburden <disburden.gamil.com>
2019-05-21 17:23:10 +0800

    add FCSettingItemWidget

--- lib/CommWidget/FCWidgetUtils.dart
+++ lib/CommWidget/FCWidgetUtils.dart
@@ -49,3 +49,86 @@ class FCWidgetUtils {
 //		);
 //	}
 //}
+
+class FCSettingItemWidget extends StatelessWidget {
+	final int index;
+	final String title;
+	final String iconFilename;
+	final String extraInfo;
+	final VoidCallback onClick;
+	final Widget subWidget;
+	final bool needRightArrow;
+	final EdgeInsets padding;
+	final FontWeight titleFontWeight;
+	
+	FCSettingItemWidget(this.title,
+		
+		{
+			this.padding = const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+			this.index = 0,
+			this.titleFontWeight,
+			this.iconFilename,
+			this.extraInfo,
+			this.onClick,
+			this.subWidget,
+			this.needRightArrow = true
+		});
+	
+	
+	
+	Widget content() {
+		return Padding(
+			padding: padding,
+			child: Row(
+				crossAxisAlignment: CrossAxisAlignment.center,
+				children: <Widget>[
+					// 图标
+					Offstage(
+						offstage: iconFilename == null,
+						child: Padding(
+							padding: const EdgeInsets.only(left: 16.0),
+							child: Image.asset('assets/images/${iconFilename}.png'),
+						),
+					),
+					
+					// 标题
+					Padding(
+						padding: const EdgeInsets.only(left: 12.0),
+						child: Text(
+							title,
+							style: TextStyle(fontSize: 16, color: Color(0xff333333), fontWeight: titleFontWeight),
+						),
+					),
+					// 附加视图 -- 尾部>图标
+					Expanded(
+						child: Row(
+							mainAxisAlignment: MainAxisAlignment.end,
+							children: <Widget>[
+								Offstage(
+									offstage: subWidget == null,
+									child: subWidget,
+								),
+								Padding(
+									padding: const EdgeInsets.only(right: 8.0),
+									child: Offstage(
+										offstage: needRightArrow == false,
+										child: Icon(
+											Icons.keyboard_arrow_right,
+											color: Color(0x282F3133),
+										),
+									),
+								),
+							],
+						),
+					),
+				],
+			),
+		);
+	}
+	
+	@override
+	Widget build(BuildContext context) {
+		//return GestureDetector(behavior: HitTestBehavior.opaque, onTap: onClick, child: content());
+		return InkWell(onTap: onClick, child: content());
+	}
+}
