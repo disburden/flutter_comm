b9d202fe8a7e035216d98b307073e236b063ede0
disburden <disburden.gamil.com>
2019-05-22 11:19:21 +0800

    FCSettingItem add backgroundcolor

--- lib/CommWidget/FCWidgetUtils.dart
+++ lib/CommWidget/FCWidgetUtils.dart
@@ -4,8 +4,6 @@ import 'dart:math' as math;
 
 
 class FCWidgetUtils {
-	
-	
 	/// 生成一个随机颜色
 	static Color sjColor() {
 		math.Random random = math.Random();
@@ -73,6 +71,7 @@ class FCSettingItemWidget extends StatelessWidget {
 	final bool needRightArrow;
 	final EdgeInsets padding;
 	final FontWeight titleFontWeight;
+	final Color backgroundColor;
 	
 	FCSettingItemWidget(this.title,
 		
@@ -84,56 +83,62 @@ class FCSettingItemWidget extends StatelessWidget {
 			this.extraInfo,
 			this.onClick,
 			this.subWidget,
-			this.needRightArrow = true
+			this.needRightArrow = true,
+			this.backgroundColor = Colors.white
 		});
 	
 	Widget content() {
-		return Padding(
-			padding: padding,
-			child: Row(
-				crossAxisAlignment: CrossAxisAlignment.center,
-				children: <Widget>[
-					// 图标
-					Offstage(
-						offstage: iconFilename == null,
-						child: Padding(
-							padding: const EdgeInsets.only(left: 16.0),
-							child: iconFilename == null ? Container() : Image.asset('assets/images/${iconFilename}.png'),
-						),
-					),
-					
-					// 标题
-					Padding(
-						padding: const EdgeInsets.only(left: 12.0),
-						child: Text(
-							title,
-							style: TextStyle(fontSize: 16, color: Color(0xff333333), fontWeight: titleFontWeight),
-						),
-					),
-					// 附加视图 -- 尾部>图标
-					Expanded(
-						child: Row(
-							mainAxisAlignment: MainAxisAlignment.end,
-							children: <Widget>[
-								Offstage(
-									offstage: subWidget == null,
-									child: subWidget,
-								),
-								Padding(
-									padding: const EdgeInsets.only(right: 8.0),
-									child: Offstage(
-										offstage: needRightArrow == false,
-										child: Icon(
-											Icons.keyboard_arrow_right,
-											color: Color(0x282F3133),
-										),
-									),
-								),
-							],
-						),
-					),
-				],
+		return Container(
+			decoration: BoxDecoration(
+				color:backgroundColor
 			),
+		  child: Padding(
+		  	padding: padding,
+		  	child: Row(
+		  		crossAxisAlignment: CrossAxisAlignment.center,
+		  		children: <Widget>[
+		  			// 图标
+		  			Offstage(
+		  				offstage: iconFilename == null,
+		  				child: Padding(
+		  					padding: const EdgeInsets.only(left: 16.0),
+		  					child: iconFilename == null ? Container() : Image.asset('assets/images/${iconFilename}.png'),
+		  				),
+		  			),
+		  			
+		  			// 标题
+		  			Padding(
+		  				padding: const EdgeInsets.only(left: 12.0),
+		  				child: Text(
+		  					title,
+		  					style: TextStyle(fontSize: 16, color: Color(0xff333333), fontWeight: titleFontWeight),
+		  				),
+		  			),
+		  			// 附加视图 -- 尾部>图标
+		  			Expanded(
+		  				child: Row(
+		  					mainAxisAlignment: MainAxisAlignment.end,
+		  					children: <Widget>[
+		  						Offstage(
+		  							offstage: subWidget == null,
+		  							child: subWidget,
+		  						),
+		  						Padding(
+		  							padding: const EdgeInsets.only(right: 8.0),
+		  							child: Offstage(
+		  								offstage: needRightArrow == false,
+		  								child: Icon(
+		  									Icons.keyboard_arrow_right,
+		  									color: Color(0x282F3133),
+		  								),
+		  							),
+		  						),
+		  					],
+		  				),
+		  			),
+		  		],
+		  	),
+		  ),
 		);
 	}
 	
