bb0635a8687a2ac129553fdec215e3cb521030d8
tyddf <tingyu@gmx.com>
2020-03-28 17:02:17 +0800

    添加FCLongButton组件

---
+++ lib/CommWidget/FCLongButton.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class FWLongButton extends StatelessWidget {
+	final String text;
+	final VoidCallback onPressed;
+	final Color bgColor;
+	final Color textColor;
+	final double textSize;
+	final double height;
+	final double roundedCornerRadius;
+	
+	FWLongButton({@required this.text,
+		@required this.onPressed,
+		this.bgColor = const Color(0x33000000),
+		this.textColor = Colors.white,
+		this.textSize = 16.0,
+		this.roundedCornerRadius = 4.0,
+		this.height = 40.0});
+	
+	@override
+	Widget build(BuildContext context) {
+		return Container(
+			child: Row(
+				children: <Widget>[
+					Expanded(
+						child: SizedBox(
+							height: height,
+							child: FlatButton(
+								onPressed: onPressed,
+								color: bgColor,
+								textColor: Colors.white,
+								shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(roundedCornerRadius)),
+								child: Text(
+									text,
+									style: TextStyle(
+										color: textColor,
+										fontSize: textSize
+									),
+								),
+							),
+						),
+					),
+				],
+			),
+		);
+	}
+}
